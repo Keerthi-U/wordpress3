0444ccdd59113fb528f7b47a8192aee006feea92
Keerthi <62284230+Keerthi-U@users.noreply.github.com>
2022-07-29 07:57:56 -0400

    contact us

---
+++ wp-content/themes/twentytwenty/contact_us.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Header file for the Twenty Twenty WordPress default theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *  Template Name: version control
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+get_header();
+
+ ?>
+<h1>HI,How Are You?</h1>
+ <?php
+get_footer();
+ ?>
